fe131b4d172164941117a91c42b77e49d0b725e9
richard jarram <rjarram@me.com>
2020-08-28 09:34:02 -0400

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'starting seeds'
+
+5.times do |i|
+    # i = 0
+    # i = 2
+    # ... i = 4
+    product = Product.create!(name: Faker::Company.name, tagline: Faker::Company.catch_phrase)
+    puts "Created product number: #{i}: name: #{product.name}"
+end
+
+puts 'seed finished'
